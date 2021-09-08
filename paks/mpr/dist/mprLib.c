56902209658631521a07f74b354732a173b4f62b
Michael O'Brien <dev@embedthis.com>
2021-09-09 07:09:32 +1000

    FIX: mprGetCurrentThread race

--- paks/mpr/dist/mprLib.c
+++ paks/mpr/dist/mprLib.c
@@ -25361,12 +25361,15 @@ PUBLIC MprThread *mprGetCurrentThread()
     ts = MPR->threadService;
     if (ts && ts->threads) {
         id = mprGetCurrentOsThread();
+        lock(ts->threads);
         for (i = 0; i < ts->threads->length; i++) {
             tp = mprGetItem(ts->threads, i);
             if (tp->osThread == id) {
+                unlock(ts->threads);
                 return tp;
             }
         }
+        unlock(ts->threads);
     }
     return 0;
 }
