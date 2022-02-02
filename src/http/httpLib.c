60ba0c53993242e20f3a979410a02e6de78225f4
Michael O'Brien <dev@embedthis.com>
2022-02-02 13:58:25 +1100

    FIX: file upload with quoted boundaries

--- src/http/httpLib.c
+++ src/http/httpLib.c
@@ -26111,6 +26111,7 @@ static Upload *allocUpload(HttpQueue *q)
     Upload      *up;
     cchar       *uploadDir;
     char        *boundary;
+    ssize       len;
 
     stream = q->stream;
     rx = stream->rx;
@@ -26125,6 +26126,13 @@ static Upload *allocUpload(HttpQueue *q)
 
     if ((boundary = strstr(rx->mimeType, "boundary=")) != 0) {
         boundary += 9;
+        if (*boundary == '"') {
+            len = slen(boundary);
+            if (boundary[len - 1] == '"') {
+                boundary[len - 1] = '\0';
+                boundary++;
+            }
+        }
         up->boundary = sjoin("--", boundary, NULL);
         up->boundaryLen = strlen(up->boundary);
     }
@@ -29620,4 +29628,3 @@ bool httpIsLastPacket(HttpPacket *packet)
     This software is distributed under a commercial license. Consult the LICENSE.md
     distributed with this software for full details and copyrights.
  */
-
